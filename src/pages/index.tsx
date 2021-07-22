3cb94c9a98598a85624be1dd63f1e6c8fe668b4d
Justin Caovan <jtcaovan@gmail.com>
2021-07-21 20:45:01 -0700

    added skeleton for project

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Justin Caovan | Front End Developer</title>
+      <p>Hi, my name is</p>
+      <h1>Justin Caovan</h1>
+      <p>Front End Developer</p>
+    </main>
+  )
+}
+
+export default IndexPage
