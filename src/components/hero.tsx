ed24bb7ea8210632a8d1923d703331c337411398
Justin Caovan <jtcaovan@gmail.com>
2021-07-24 21:33:41 -0700

    installed Framer motion and added example animation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { motion } from "framer-motion"
 
 const Hero = () => {
     return (
         <div className="font-display flex flex-col justify-center items-center w-full h-screen m-auto 
-        bg-bg-blue text-white">
-            <h1 className=''>JUSTIN CAOVAN</h1>
-            <p>Front End Developer</p>
+        bg-bg-blue text-white tracking-wider text-center">
+            <motion.div className='border p-4'
+                    animate={{ scale: 1.5 }}
+                    transition={{ duration: 0.5 }}
+                    whileHover={{ scale: 2 }}
+                    >
+                <h1 className=''>JUSTIN CAOVAN</h1>
+                <p>Front End Developer</p>
+            </motion.div>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,13 @@ import Contact from '../components/contact'
 
 const IndexPage = () => {
   return (
-    <main>
+    <>
       <title>Justin Caovan | Front End Developer</title>
       <Hero />
       <About />
       <Projects />
       <Contact />
-    </main>
+    </>
   )
 }
 
