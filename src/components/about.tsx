a4e6440bd4013f6908c0654f4a8a98a4d980e5e4
Justin Caovan <jtcaovan@gmail.com>
2021-07-27 14:47:10 -0700

    styling adjustments and new logo

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,10 +1,10 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {javascript, html, css, react, redux, gatsby, tailwind, webpack, vscode, graphql, git, npm, heroku, netlify} from '../images/icons'
 import { motion, useAnimation, useViewportScroll, useTransform } from "framer-motion"
 import { useInView } from 'react-intersection-observer'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const About = () => {
-
     const {ref, inView} = useInView({
         threshold: 0.2
     })
@@ -22,51 +22,47 @@ const About = () => {
         if(!inView) {
             animation.start({
                 x: '-100vw',
-            transition: {
-                duration: 4
+                transition: {
+                    duration: 2
             } })
         }
     }, [inView])
 
     return (
-        <div ref={ref} className='bg-gradient-to-t from-gray-700 via-gray-600 to-gray-600 text-white h-full w-full py-28 overflow-hidden'>
-            <motion.div className='flex flex-col items-center justify-center m-auto space-y-20' animate={animation}>
+        <div ref={ref} className='bg-gradient-to-t from-gray-700 via-gray-600 to-gray-600 text-white h-full w-full pt-12 pb-28 overflow-hidden'>
+            <motion.div className='flex mx-20 flex-col' animate={animation}>
+                    <h1 className='font-display text-4xl font-thin'>About Me</h1>
+                    <div className='flex items-center justify-around'>
+                        <p className='w-1/2 font-body tracking-wide leading-loose'>I'm a self-taught Front-end developer based in Phoenix, Arizona</p>
+                        <StaticImage className='w-96' src='../images/profile.svg' alt='profile'/>
+                    </div>
 
-                <div className='w-7/12 text-center flex flex-col space-y-4'>
-                <h1 className='font-display text-4xl font-thin'>About Me</h1>
-                    <p className='font-body tracking-wide leading-loose'>I'm a self-taught Front-end developer based in Phoenix, Arizona 
-                    I'm a self-taught Front-end developer based in Phoenix, Arizona
-                    I'm a self-taught Front-end developer based in Phoenix, Arizona
-                    I'm a self-taught Front-end developer based in Phoenix, Arizona
-                    I'm a self-taught Front-end developer based in Phoenix, Arizona
-                    I'm a self-taught Front-end developer based in Phoenix, Arizona</p>
-                </div>
-
-                <div className='flex w-2/5 justify-between'>
-                    <div className='text-center w-2/5'>
+                <div className='flex flex-col space-y-8 w-2/5 justify-between'>
+                    <h1 className='font-display text-4xl font-thin'>Skills and Languages</h1>
+                    <div className='w-max rounded-xl'>
                         <h2 className='font-display text-xl font-thin'>Technologies</h2>
-                        <ul className='grid grid-cols-2 gap-2 font-body bg-white 
-                        text-black border rounded-lg border-black p-4'>
-                            <motion.li whileHover={{ scale: 1.3 }}>{html} HTML5</motion.li>
-                            <li>{css} CSS3</li>
-                            <li>{javascript}Javascript</li>
-                            <li>{react}React</li>
-                            <li>{redux}Redux</li>
-                            <li>{gatsby}Gatsby</li>
-                            <li>{tailwind}Tailwind CSS</li>
+                        <ul className='flex space-x-8 font-body 
+                         px-8 py-4 shadow-2xl rounded-xl'>
+                            <li className='icon'>{html} HTML5</li>
+                            <li className='icon'>{css} CSS3</li>
+                            <li className='icon'>{javascript}Javascript</li>
+                            <li className='icon'>{react}React</li>
+                            <li className='icon'>{redux}Redux</li>
+                            <li className='icon'>{gatsby}Gatsby</li>
+                            <li className='icon'>{tailwind}Tailwind</li>
                         </ul>
                     </div>
-                    <div className='text-center w-2/5'>
+                    <div className='w-max'>
                         <h2 className='font-display text-xl font-thin'>Tools</h2>
-                        <ul className='grid grid-cols-2 gap-2 font-body bg-white 
-                        text-black border rounded-lg border-black p-4'>
-                            <li>{git}Git</li>
-                            <li>{vscode}VS Code</li>
-                            <li>{heroku}Heroku</li>
-                            <li>{netlify}Netlify</li>
-                            <li>{webpack}Webpack</li>
-                            <li>{graphql}GraphQL</li>
-                            <li>{npm}npm</li>
+                        <ul className='flex space-x-8 font-body 
+                         px-8 py-4 shadow-2xl rounded-xl'>
+                            <li className='icon'>{git}Git</li>
+                            <li className='icon'>{vscode}VS Code</li>
+                            <li className='icon'>{heroku}Heroku</li>
+                            <li className='icon'>{netlify}Netlify</li>
+                            <li className='icon'>{webpack}Webpack</li>
+                            <li className='icon'>{graphql}GraphQL</li>
+                            <li className='icon'>{npm}npm</li>
                         </ul>
                     </div>
                 </div>
