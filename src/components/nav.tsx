930b7c5c155d833e1f6d6e63fb60fea94cfa075e
Justin Caovan <jtcaovan@gmail.com>
2021-07-26 22:25:21 -0700

    installed gatsby image plugin and added logo

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Nav = () => {
     return (
         <div className=''>
             <ul className='flex m-auto justify-end absolute inset-x-0 top-0 text-white font-thin w-full h-16'>
-                <li >Logo</li>
+                <li><StaticImage className='w-14 border-2' src='../images/logo.svg' alt='Logo'/>Logo</li>
                 <li>About</li>
                 <li>Projects</li>
                 <li>Contact</li>
