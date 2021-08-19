6201cb0a758b530bfa9d2db1a27afbdee17e92dc
Justin Caovan <jtcaovan@gmail.com>
2021-08-18 20:32:34 -0700

    Added link to Live Demo icon and hover title

--- src/components/portfolio/ProjectCard.tsx
+++ src/components/portfolio/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import github from "../../images/github.svg"
+import demo from '../../images/demo.svg'
 
 function ProjectCard(prop) {
     let tags = prop.tags.map(tag => 
@@ -14,9 +15,9 @@ function ProjectCard(prop) {
             <p className='tracking-wider text-3xl'>{prop.name}</p>
             <ul className='flex space-x-2 m-auto justify-center opacity-85'>{tags}</ul>
             <p className='text-lg w-11/12 m-auto leading-loose'>{prop.description}</p>
-            <div className='absolute top-0 right-0 flex space-x-2'>
-                <a href={prop.github} target='_blank'><img className='h-6 w-6' src={github} /></a>
-                <button className='border border-light-green rounded-md p-1'><a href={prop.liveDemo}> Demo</a></button>
+            <div className='absolute top-2 right-1 flex space-x-2'>
+                <a title='Source Code' href={prop.github} target='_blank'><img className='h-6 w-6 hover:scale-125 transition duration-150' src={github} /></a>
+                <a title='Live Demo' href={prop.liveDemo} target='_blank'><img className='h-6 w-6 hover:scale-125 transition duration-150' src={demo} /></a>
             </div>
         </div>
     )
