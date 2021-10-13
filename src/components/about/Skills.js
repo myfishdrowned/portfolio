c9ff1a5775a221bcc730f93d11e60d7dc1562a75
Justin Caovan <jtcaovan@gmail.com>
2021-10-13 12:32:02 -0700

    Updated Resume and fixed Skills layout

--- src/components/about/Skills.js
+++ src/components/about/Skills.js
@@ -53,37 +53,37 @@ function Skills() {
                 </motion.p>
             </div>
 
-                <div className='flex flex-col w-full text-center space-y-24 lg:flex-row space-y-12 lg:space-y-0 lg:space-x-12 lg:divide-x divide-opacity-30 divide-graphite text-center justify-between'>
-                    <motion.div className='w-full lg:w-1/2'           
+            <div className='flex flex-col w-full text-center space-y-24 lg:flex-row space-y-12 lg:space-y-0 lg:space-x-12 lg:divide-x divide-opacity-30 divide-graphite text-center justify-between'>
+                <motion.div className='w-full lg:w-1/2'           
+                    initial='hidden'
+                    animate={animation}
+                    variants={header}>
+                    <motion.h2 variants={header} className='font-display text-lg'>Languages and Frameworks</motion.h2>
+                    <motion.ul variants={header} className='grid grid-rows-2 grid-cols-3 gap-y-16 justify-items-center font-body text-sm pt-14'>
+                        <motion.li variants={icons} className='icon'>{html} HTML5</motion.li>
+                        <motion.li variants={icons} className='icon'>{css} CSS3</motion.li>
+                        <motion.li variants={icons} className='icon'>{javascript}JavaScript</motion.li>
+                        <motion.li variants={icons} className='icon'>{typescript}TypeScript</motion.li>
+                        <motion.li variants={icons} className='icon'>{react}React</motion.li>
+                        <motion.li variants={icons} className='icon'>{gatsby}Gatsby</motion.li>
+                        <motion.li variants={icons} className='icon'>{tailwind}Tailwind</motion.li>
+                    </motion.ul>
+                </motion.div>
+                <motion.div className='w-full lg:w-1/2'
                         initial='hidden'
                         animate={animation}
                         variants={header}>
-                        <motion.h2 variants={header} className='font-display text-lg'>Languages and Frameworks</motion.h2>
-                        <motion.ul variants={header} className='grid grid-rows-2 grid-cols-3 gap-y-16 justify-items-center font-body text-sm pt-14'>
-                            <motion.li variants={icons} className='icon'>{html} HTML5</motion.li>
-                            <motion.li variants={icons} className='icon'>{css} CSS3</motion.li>
-                            <motion.li variants={icons} className='icon'>{javascript}JavaScript</motion.li>
-                            <motion.li variants={icons} className='icon'>{typescript}TypeScript</motion.li>
-                            <motion.li variants={icons} className='icon'>{react}React</motion.li>
-                            <motion.li variants={icons} className='icon'>{gatsby}Gatsby</motion.li>
-                            <motion.li variants={icons} className='icon'>{tailwind}Tailwind</motion.li>
-                        </motion.ul>
-                    </motion.div>
-                    <motion.div className='w-full lg:w-1/2'
-                            initial='hidden'
-                            animate={animation}
-                            variants={header}>
-                        <motion.h2 variants={header} className='font-display text-lg'>Development Tools</motion.h2>
-                        <motion.ul variants={header} className='grid grid-rows-2 grid-cols-3 gap-y-16 justify-items-center font-body text-sm pt-14'>
-                            <motion.li variants={icons} className='icon'>{redux}Redux</motion.li>
-                            <motion.li variants={icons} className='icon'>{webpack}Webpack</motion.li>
-                            <motion.li variants={icons} className='icon'>{framer}Framer</motion.li>
-                            <motion.li variants={icons} className='icon'>{heroku}Heroku</motion.li>
-                            <motion.li variants={icons} className='icon'>{netlify}Netlify</motion.li>
-                            <motion.li variants={icons} className='icon'>{npm}npm</motion.li>
-                        </motion.ul>
-                    </motion.div>
-                </div>
+                    <motion.h2 variants={header} className='font-display text-lg'>Development Tools</motion.h2>
+                    <motion.ul variants={header} className='grid grid-rows-2 grid-cols-3 gap-y-16 justify-items-center font-body text-sm pt-14'>
+                        <motion.li variants={icons} className='icon'>{redux}Redux</motion.li>
+                        <motion.li variants={icons} className='icon'>{webpack}Webpack</motion.li>
+                        <motion.li variants={icons} className='icon'>{framer}Framer</motion.li>
+                        <motion.li variants={icons} className='icon'>{heroku}Heroku</motion.li>
+                        <motion.li variants={icons} className='icon'>{netlify}Netlify</motion.li>
+                        <motion.li variants={icons} className='icon'>{npm}npm</motion.li>
+                    </motion.ul>
+                </motion.div>
+            </div>
         </div>
     )
 }
