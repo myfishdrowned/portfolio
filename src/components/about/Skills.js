59bf429ed263e81ba9811b9b6a17852b75906dc6
myfishdrowned <saif.rahman.va@gmail.com>
2025-07-10 17:43:41 -0400

    updated skills component to reflect new file path for icons

--- src/components/about/Skills.js
+++ src/components/about/Skills.js
@@ -1,7 +1,21 @@
 import React, {useEffect} from 'react'
 import { motion, useAnimation } from "framer-motion"
 import { useInView } from 'react-intersection-observer'
+import awsIcon from '../../images/aws.png';
+import beanshellIcon from '../../images/beanshell.png';
+import identityiqIcon from '../../images/identityiq.png';
+import identitynowIcon from '../../images/identitynow.png';
+import javaIcon from '../../images/java.png';
+import jiraIcon from '../../images/jira.png';
 import postmanIcon from '../../images/postman.png';
+import ldapIcon from '../../images/ldap.png';
+import oktaIcon from '../../images/okta.png';
+import rbacIcon from '../../images/rbac.png';
+import sailpointIcon from '../../images/sailpoint.png';
+import secretserverIcon from '../../images/secretserver.png';
+import servicenowIcon from '../../images/servicenow.png';
+import sqlIcon from '../../images/sql.png';
+import ssoIcon from '../../images/sso.png';
 
 // Add this style block for the skill-icon class
 const style = `
@@ -72,39 +86,39 @@ function Skills() {
                         <motion.ul variants={header} className='grid grid-rows-2 grid-cols-3 gap-y-16 justify-items-center font-body text-sm pt-14'>
                             {/* SailPoint skill styled like the sample: icon above, text below, centered, no background circle */}
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/sailpoint.png" alt="SailPoint" className="skill-icon mb-2" />
+                                <img src={sailpointIcon} alt="SailPoint" className="skill-icon mb-2" />
                                 <span>SailPoint</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/identitynow.png" alt="IdentityNOW" className="skill-icon mb-2" />
+                                <img src={identitynowIcon} alt="IdentityNOW" className="skill-icon mb-2" />
                                 <span>IdentityNOW</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/identityiq.png" alt="IdentityIQ" className="skill-icon mb-2" />
+                                <img src={identityiqIcon} alt="IdentityIQ" className="skill-icon mb-2" />
                                 <span>IdentityIQ</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/okta.png" alt="Okta" className="skill-icon mb-2" />
+                                <img src={oktaIcon} alt="Okta" className="skill-icon mb-2" />
                                 <span>Okta</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/secretserver.png" alt="Secret Server" className="skill-icon mb-2" style={{width: '56px', height: '56px'}} />
+                                <img src={secretserverIcon} alt="Secret Server" className="skill-icon mb-2" style={{width: '56px', height: '56px'}} />
                                 <span>Secret Server</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/servicenow.png" alt="ServiceNOW" className="skill-icon mb-2" />
+                                <img src={servicenowIcon} alt="ServiceNOW" className="skill-icon mb-2" />
                                 <span>ServiceNOW</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/aws.png" alt="AWS" className="skill-icon mb-2" />
+                                <img src={awsIcon} alt="AWS" className="skill-icon mb-2" />
                                 <span>AWS</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/jira.png" alt="JIRA" className="skill-icon mb-2" />
+                                <img src={jiraIcon} alt="JIRA" className="skill-icon mb-2" />
                                 <span>JIRA</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/activedirectory.png" alt="Active Directory" className="skill-icon mb-2" />
+                                <img src={ldapIcon} alt="Active Directory" className="skill-icon mb-2" />
                                 <span>Active Directory</span>
                             </motion.li>
                         </motion.ul>
@@ -116,11 +130,11 @@ function Skills() {
                         <motion.h2 variants={header} className='font-display text-lg'>Development & Tools</motion.h2>
                         <motion.ul variants={header} className='grid grid-rows-2 grid-cols-3 gap-y-16 justify-items-center font-body text-sm pt-14'>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/java.png" alt="Java" className="skill-icon mb-2" />
+                                <img src={javaIcon} alt="Java" className="skill-icon mb-2" />
                                 <span>Java</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/powershell.png" alt="PowerShell" className="skill-icon mb-2" />
+                                <img src={beanshellIcon} alt="PowerShell" className="skill-icon mb-2" />
                                 <span>PowerShell</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
@@ -128,23 +142,23 @@ function Skills() {
                                 <span>Postman</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/sql.png" alt="SQL" className="skill-icon mb-2" />
+                                <img src={sqlIcon} alt="SQL" className="skill-icon mb-2" />
                                 <span>SQL</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/beanshell.png" alt="Beanshell" className="skill-icon mb-2" />
+                                <img src={beanshellIcon} alt="Beanshell" className="skill-icon mb-2" />
                                 <span>Beanshell</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/ldap.png" alt="LDAP" className="skill-icon mb-2" />
+                                <img src={ldapIcon} alt="LDAP" className="skill-icon mb-2" />
                                 <span>LDAP</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/rbac.png" alt="RBAC" className="skill-icon mb-2" />
+                                <img src={rbacIcon} alt="RBAC" className="skill-icon mb-2" />
                                 <span>RBAC</span>
                             </motion.li>
                             <motion.li variants={icons} className='flex flex-col items-center'>
-                                <img src="/sso.png" alt="SSO" className="skill-icon mb-2" />
+                                <img src={ssoIcon} alt="SSO" className="skill-icon mb-2" />
                                 <span>SSO</span>
                             </motion.li>
                             <motion.li variants={icons} className='px-2 py-1 text-sm border bg-gray-300 bg-opacity-60 border-graphite border-opacity-20 rounded-full cursor-default transition transform duration-400 ease-in-out hover:scale-110'>JML Processes</motion.li>
