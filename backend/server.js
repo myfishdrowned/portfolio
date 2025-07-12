821a210bd5de775b6c905d0baa82165883d9d271
myfishdrowned <saif.rahman.va@gmail.com>
2025-07-12 12:18:57 -0400

    removed hardcoded port value

--- backend/server.js
+++ backend/server.js
@@ -55,4 +55,7 @@ app.use((err, req, res, next) => {
   });
 });
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 module.exports = app; 
